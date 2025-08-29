7c2d1ea10c0d1d2a5c6a790069f62f9402d18c07
Tarun Kumar <tarunkumarsingh295@gmail.com>
2025-08-29 22:18:36 +0530

    Updated components and added team images

--- src/components/ImpactSection.jsx
+++ src/components/ImpactSection.jsx
@@ -165,8 +165,8 @@ export default function ImpactSection() {
             },
             {
               value: "30+",
-              label: "Schools Engaged",
-              desc: "Inspiring students to care for the environment",
+              label: "Villages Engaged",
+              desc: "in projects that bring people together towards progress",
             },
           ].map((item, idx) => (
             <ImpactCard
